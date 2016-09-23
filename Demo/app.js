c115ec2c231bd84643e648b15a7f85579bd0ca2d
snowflake2016 <xiaxia2525@gmail.com>
2016-09-24 06:54:26 +0800

    HTML基礎 274期
    Final Demo

---
+++ Demo/app.js
@@ -0,0 +1,89 @@
+$(document).ready(function () {
+    $(".hw1").mouseover(function () {
+        $(".hw1").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h1").hide();
+        $(".p1").hide();
+    });
+    $(".hw1").mouseout(function () {
+        $(".hw1").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h1").show();
+        $(".p1").show();
+    });
+
+    $(".hw2").mouseover(function () {
+        $(".hw2").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h2").hide();
+        $(".p2").hide();
+    });
+    $(".hw2").mouseout(function () {
+        $(".hw2").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h2").show();
+        $(".p2").show();
+    });
+
+    $(".hw3").mouseover(function () {
+        $(".hw3").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h3").hide();
+        $(".p3").hide();
+    });
+    $(".hw3").mouseout(function () {
+        $(".hw3").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h3").show();
+        $(".p3").show();
+    });
+
+    $(".hw4").mouseover(function () {
+        $(".hw4").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h4").hide();
+        $(".p4").hide();
+    });
+    $(".hw4").mouseout(function () {
+        $(".hw4").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h4").show();
+        $(".p4").show();
+    });
+
+    $(".hw5").mouseover(function () {
+        $(".hw5").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h5").hide();
+        $(".p5").hide();
+    });
+    $(".hw5").mouseout(function () {
+        $(".hw5").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h5").show();
+        $(".p5").show();
+    });
+
+    $(".hw6").mouseover(function () {
+        $(".hw6").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h6").hide();
+        $(".p6").hide();
+    });
+    $(".hw6").mouseout(function () {
+        $(".hw6").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h6").show();
+        $(".p6").show();
+    });
+
+    $(".hw7").mouseover(function () {
+        $(".hw7").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h7").hide();
+        $(".p7").hide();
+    });
+    $(".hw7").mouseout(function () {
+        $(".hw7").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h7").show();
+        $(".p7").show();
+    });
+
+    $(".hw8").mouseover(function () {
+        $(".hw8").css("-webkit-filter", "blur(5px)", "filter", "blur(5px)");
+        $(".h8").hide();
+        $(".p8").hide();
+    });
+    $(".hw8").mouseout(function () {
+        $(".hw8").css("-webkit-filter", "blur(0px)", "filter", "blur(0px)");
+        $(".h8").show();
+        $(".p8").show();
+    });
+});
